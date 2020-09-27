9ae14e1a43ad9bdbbabaf33bb39d514f3efc6614
Bryan <bryan87reed@gmail.com>
2020-09-26 19:54:28 -0700

    Removed comment

--- Scripts/ActiveBountyProgress.gd
+++ Scripts/ActiveBountyProgress.gd
@@ -6,7 +6,6 @@ func _on_Timer_timeout():
 func on_click():
 	print("Click")
 	
-# TODO figure out why this isn't firing
 func _on_ActiveBountyProgress_input_event(viewport, event, shape_idx):
 	if event.is_action_released("left_click"):
 		self.on_click()
