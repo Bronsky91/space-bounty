cfbd595fb38c3b183dfba4b5071be14c896a7c6a
Bryan <bryan87reed@gmail.com>
2020-12-05 14:54:09 -0700

    Fixed hair sort for animations

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -6,3 +6,8 @@ func _ready():
 func _process(delta):
 	pass
 
+func _on_AnimationPlayer_animation_started(anim_name):
+	if 'Down' in anim_name:
+		move_child($HairB, 0)
+	else:
+		move_child($HairB, 7)
